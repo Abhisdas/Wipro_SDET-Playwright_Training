1e9b6f9c9eecd4cd690261f68df58f00c758dd99
Abhisdas <dashabhish@gmail.com>
2026-05-05 16:41:56 +0530

    practice qsns

--- Day_4/code.js
+++ Day_4/code.js
@@ -1,32 +1,34 @@
 function getData() {
-  return new Promise((resolve, reject) => {
-    console.log("Fetching data...");
-
-    setTimeout(() => {
-      const success = true; 
-      if (success) {
-        resolve("Data received");
-      } else {
-        reject("Failed to fetch data");
-      }
-    }, 2000);
-  });
+    return new Promise((resolve, reject) => {
+        if(2 > 3) {
+ 
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
+        } else {
+            reject('not working')
+        }
+    });
 }
-
-getData()
-  .then((data) => {
-    console.log("Success:", data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-
-
-
-setTimeout(() => {
-  console.log("This runs after 5 seconds");
-}, 5000);
-
-setInterval(() => {
-  console.log("Checking for new data...");
-}, 3000);
+ 
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
+ 
+// getData().then((data) => {
+//     console.log(data);
+// }).catch((error) => {
+//     console.error("Error:", error);
+// });
+ 
+// setTimeout(() => {
+//     console.log("Data received");
+// }, 5000);
+// console.log("test");
+ 
+// setInterval(() => {
+//     console.log("Checking for new data...");
+// }, 3000);
